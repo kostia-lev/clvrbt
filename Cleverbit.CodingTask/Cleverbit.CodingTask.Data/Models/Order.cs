f6e28035ed102ac3ecf86fb4c63fab0759f89f6e
vahrushev <k.vakhrushev@svitla.com>
2024-02-02 18:48:09 +0200

    files

---
+++ Cleverbit.CodingTask/Cleverbit.CodingTask.Data/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Cleverbit.CodingTask.Data.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ Cleverbit.CodingTask/Cleverbit.CodingTask.Data/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cleverbit.CodingTask.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
